c029277d848f347001521f560018425b35f48906
Justin Caryk <cary9983@gmail.com>
2017-12-14 19:09:45 -0800

    added gulp/babel

---
+++ content/build/all.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+    'use strict';
+
+    angular.module('client', [
+    //3rd party
+    'ui.router', 'ui.bootstrap',
+
+    //services
+    'client.services',
+
+    //views & controllers
+    'client.site', 'client.crud']);
+
+    angular.module('client').config(RouteConfig).run(StateErrorHandler);
+
+    StateErrorHandler.$inject = ['$rootScope', '$log'];
+
+    function StateErrorHandler($rootScope, $log) {
+        $rootScope.$on('$stateChangeError', function (info) {
+            return $log.log(info);
+        });
+    }
+
+    RouteConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+    function RouteConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise('/otherwise');
+        $locationProvider.html5mode(true);
+    }
+})();

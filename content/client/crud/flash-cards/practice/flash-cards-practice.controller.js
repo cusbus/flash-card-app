916182e530526c3f9b1596be381c434dd67e1fc3
Justin Caryk <cary9983@gmail.com>
2017-12-19 17:20:22 -0800

    -basic carousel functionality implemented

--- content/client/crud/flash-cards/practice/flash-cards-practice.controller.js
+++ content/client/crud/flash-cards/practice/flash-cards-practice.controller.js
@@ -11,19 +11,54 @@
         let vm = this
         vm.toggleQuestion = true
         vm.toggleAnswer = false
+        
+        //carousel vars 
+        vm.currentIndex = null
+        vm.currentFlashCard = null
 
         //public functions
         vm.toggleQA = _toggleQA
+        vm.updateCarouselIndex = _updateCarouselIndex
+        vm.refreshCarouselCard = _refreshCarouselCard
 
         init()
 
         function init(){
-            vm.flashCards = flashCards
+            vm.currentFlashCard = flashCards[0]
+            vm.currentIndex = 0
         }
 
         function _toggleQA(){
             vm.toggleAnswer = !vm.toggleAnswer
             vm.toggleQuestion = !vm.toggleQuestion
+        }
+
+        function _updateCarouselIndex (direction) {
+            //handling previous click
+            if (direction == 'previous') {
+                if (vm.currentIndex === 0) {
+                    vm.currentIndex = flashCards.length - 1
+                }
+                else { 
+                    vm.currentIndex = vm.currentIndex - 1
+                }
+            }
+            //handling next click
+            if (direction == 'next') {
+                if (vm.currentIndex == flashCards.length -1) {
+                    vm.currentIndex = 0
+                }
+                else { 
+                    vm.currentIndex = vm.currentIndex + 1
+                }
+                
+            }
+        }
+
+        function _refreshCarouselCard(qOrA){
+            vm.currentFlashCard = flashCards[vm.currentIndex]
+            if (qOrA == 'Q') { return vm.currentFlashCard.question }
+            if (qOrA == 'A') { return vm.currentFlashCard.answer }
         }
     }
 
